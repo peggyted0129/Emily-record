d470fe5fed5385bf55fa708c4183009b2f40ad59
peggyted0129 <peggyted0129@gamil.com>
2021-02-08 23:27:57 +0800

    fix OrderPaid.vue

--- src/views/front/OrderPaid.vue
+++ src/views/front/OrderPaid.vue
@@ -8,16 +8,18 @@
         <li class="breadcrumb-item font-weight-bolder active" aria-current="page">付款完成</li>
       </ol>
     </nav>
-    <ul class="finish d-flex flex-column justify-content-center align-items-center mt-10">
-      <li class="mb-9"><i class="fas fa-truck"></i></li>
-      <li class="mb-9"><h3 class="h2">恭喜您，已完成付款結帳囉 ！</h3></li>
-      <li class="mb-9">
-        <p class="h5">感謝您對 <strong>Emily Cake</strong> 的信任，商品將會在 1 - 3 天送到，請耐心等候。</p>
-      </li>
-      <li class="text-center w-100 mb-9">
-        <router-link to="/products" class="btn btn-warning font-weight-bolder w-50">繼續購物</router-link>
-      </li>
-    </ul>
+    <div class="finish-banner d-flex  align-items-center justify-content-center mb-12">
+      <ul class="finish d-flex flex-column justify-content-center align-items-center p-13">
+        <li class="mb-7"><i class="fas fa-truck"></i></li>
+        <li class="mb-7"><h3 class="h2">交易成功 ！</h3></li>
+        <li class="mb-7">
+          <p class="h5">感謝您的訂購，歡迎再度光臨選購。</p>
+        </li>
+        <li class="text-center w-100">
+          <router-link to="/products" class="btn btn-warning font-weight-bolder w-75">繼續購物</router-link>
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 <script>
